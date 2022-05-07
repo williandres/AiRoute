52777dd2f11340b4c54dc023777d133752d28cac
Willian <sca.willian@gmail.com>
2022-05-06 22:12:14 -0500

    Dict and list comprenhensions

--- INTERMEDIATE/lists_and_dicts.py
+++ INTERMEDIATE/lists_and_dicts.py
@@ -1,13 +1,13 @@
 def run():
     my_list = [1, "Hello", True, 4.5]
-    my_dict = {"firstname": "Facundo", "lastname": "GarcÃ­a"}
+    my_dict = {"firstname": "Facundo", "lastname": "Lanon"}
 
     super_list = [
         {"firstname": "Facundo", "lastname": "Mondongo"},
         {"firstname": "Miguel", "lastname": "Rodriguez"},
         {"firstname": "Pablo", "lastname": "Trinidad"},
         {"firstname": "Susana", "lastname": "Martinez"},
-        {"firstname": "JosÃ©", "lastname": "Fernandez"},
+        {"firstname": "Josi", "lastname": "Fernandez"},
     ]
 
     super_dict = {
@@ -19,9 +19,19 @@ def run():
     for key, value in super_dict.items():
         print(key, ">", value)
 
-    for i in range (0,len(super_list)):
+    for i in range (0, len(super_list)):
         print(super_list[i])
 
+    """for values in super_list:
+            for key, value in values.items():
+                print(f'{key} - {value}')"""
+
+    """for i in super_list:
+            for key, values in i.items():
+                print(key,": ", values);"""
+
+    """for i in super_list:
+	        print(i.items())"""
 
 if __name__ == '__main__':
     run()
