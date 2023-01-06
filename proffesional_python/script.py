15e49d851ffa048fc02aadc42a6e6e15ede0e681
Willian Cárdenas <sca.willian@gmail.com>
2023-01-05 20:48:28 -0500

    testing venvs and matplot lib

---
+++ proffesional_python/script.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+def generate_chart():
+    #data = read_csv.read_csv('./data/lb.csv')
+    plt.savefig('./content/developers.png')
+    plt.close()
+
+
+
+if __name__ == '__main__':
+    generate_chart()
+
+
+
