348956755fb5a641dbab071f86907cc860f516af
Willian Cárdenas <sca.willian@gmail.com>
2023-01-11 00:33:40 -0500

    Testin map function

--- proffesional_python/script.py
+++ proffesional_python/script.py
@@ -1,14 +1,28 @@
-import matplotlib.pyplot as plt
+items = [
+    {
+        'product': 'camisa',
+        'price': 100,
+    },
+    {
+        'product': 'pantalones',
+        'price': 300
+    },
+    {
+        'product': 'pantalones 2',
+        'price': 200
+    }
+]
 
-def generate_chart():
-    #data = read_csv.read_csv('./data/lb.csv')
-    plt.savefig('./content/developers.png')
-    plt.close()
 
+def add_taxes(item):
+    item = item.copy() # To not modify the original list
+    item['taxes'] = item['price'] * .19
+    item['price'] = item['price'] + 1
 
+    return item
 
-if __name__ == '__main__':
-    generate_chart()
-
-
+new_items = list(map(add_taxes, items))
+print(items)
+print("-----"*5)
+print(new_items)
 
