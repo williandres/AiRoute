6ee0de0dfebcde3974fb20d62aabe84dffe9e578
Willian Cárdenas <sca.willian@gmail.com>
2023-01-11 00:47:11 -0500

    a simple map function

--- proffesional_python/script.py
+++ proffesional_python/script.py
@@ -1,28 +1,8 @@
-items = [
-    {
-        'product': 'camisa',
-        'price': 100,
-    },
-    {
-        'product': 'pantalones',
-        'price': 300
-    },
-    {
-        'product': 'pantalones 2',
-        'price': 200
-    }
-]
+def multiply_numbers(numbers):
+    result = list(map(lambda x: x*2, numbers))
+    return result
 
-
-def add_taxes(item):
-    item = item.copy() # To not modify the original list
-    item['taxes'] = item['price'] * .19
-    item['price'] = item['price'] + 1
-
-    return item
-
-new_items = list(map(add_taxes, items))
-print(items)
-print("-----"*5)
-print(new_items)
+numbers = [2, 4, 5, 6, 8]
+response = multiply_numbers(numbers)
+print(response)
 
